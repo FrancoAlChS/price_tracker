bd169609cf3cc96e8fa77c3454cf5bb7df5aa827
FrancoAlChS <chipanasiguas@gmail.com>
2026-06-14 18:34:15 -0500

    feat: :sparkles: first commit

---
+++ src/config/enviroments.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
+    raise ValueError("❌ ERROR: Faltan configurar las variables de entorno en el archivo .env")
